3c121c69f8249e7d7ac632f6ae9b4a94cdb9066b
prlakshm <pranavi_lakshminarayanan@brown.edu>
2024-01-02 10:33:48 -0500

    Feat: getting ready to deploy

--- repl/src/frontend/components/App.tsx
+++ repl/src/frontend/components/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import REPL from './REPL';
-import '../styles/App.css';
+import React, { useState } from "react";
+import REPL from "./REPL";
+import "../styles/App.css";
 
 function App() {
   const [isPanelOpen, setIsPanelOpen] = useState(true);
@@ -11,48 +11,66 @@ function App() {
 
   return (
     <div className="App">
-      <div className={`App-header ${isPanelOpen ? 'panel-open' : 'panel-closed'}`}>
-        <h1 style={{ marginTop: '4px', marginBottom: '10px' }}>REPL</h1>
+      <div
+        className={`App-header ${isPanelOpen ? "panel-open" : "panel-closed"}`}
+      >
+        <h1 style={{ marginTop: "4px", marginBottom: "10px" }}>REPL</h1>
         {isPanelOpen && (
           <>
             <div>
-              <span style={{ color: 'lightblue' }}>"load [file_path]"</span> to load a csv file
+              <span style={{ color: "lightblue" }}>"load [file_path]"</span> to
+              load a csv file
             </div>
             <div>
-              <span style={{ color: 'lightblue' }}>"view"</span> to view csv
+              <span style={{ color: "lightblue" }}>"view"</span> to view csv
             </div>
             <div>
-              <span style={{ color: 'lightblue' }}>
+              <span style={{ color: "lightblue" }}>
                 "search [has_headers] [search_value] [column_identifier]"
-              </span>{' '}
-              to search column csv where has_headers is true/false and column_identifier is column name/column index
+              </span>{" "}
+              to search column csv where{" "}
+              <span style={{ color: "lightblue" }}>has_headers</span> is
+              true/false and{" "}
+              <span style={{ color: "lightblue" }}>column_identifier</span> is
+              column name/column index
             </div>
             <div>
-              <span style={{ color: 'lightblue' }}>"search [has_headers] [search_value] *"</span>{' '}
-              to search all columns csv where has_headers is true/false
+              <span style={{ color: "lightblue" }}>
+                "search [has_headers] [search_value] *"
+              </span>{" "}
+              to search all columns csv where{" "}
+              <span style={{ color: "lightblue" }}>has_headers</span> is
+              true/false
             </div>
             <div>
-              <span style={{ color: 'lightblue' }}>"broadband [state] [county]"</span>{' '}
+              <span style={{ color: "lightblue" }}>
+                "broadband [state] [county]"
+              </span>{" "}
               to get cached broadband access percent of county in state
             </div>
             <div>
-              <span style={{ color: 'lightblue' }}>"mode brief"</span> to display only history output or{' '}
-              <span style={{ color: 'lightblue' }}>"mode verbose"</span> to display both history command and output
+              <span style={{ color: "lightblue" }}>"mode brief"</span> to
+              display only history output or{" "}
+              <span style={{ color: "lightblue" }}>"mode verbose"</span> to
+              display both history command and output
             </div>
             <div>
-              <span style={{ color: 'lightblue' }}>
+              <span style={{ color: "lightblue" }}>
                 "register [command] [function_to_execute]"
-              </span>{' '}
+              </span>{" "}
               to register a new command
             </div>
           </>
         )}
-        <button onClick={togglePanel} style={{marginBottom: "10px", border: "1px solid white"}}>
-          {isPanelOpen ? 'Close Instructions' : 'Open Instructions'}
+        <button
+          onClick={togglePanel}
+          style={{ marginBottom: "10px", border: "1px solid white" }}
+        >
+          {isPanelOpen ? "Close Instructions" : "Open Instructions"}
         </button>
       </div>
       <div className="main-content">
-        <REPL isPanelOpen={isPanelOpen}/>
+        <REPL isPanelOpen={isPanelOpen} />
       </div>
     </div>
   );
